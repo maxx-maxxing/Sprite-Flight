c85576c51d8289243836d1dec6002de85b6849dd
maxx-maxxing <maxxlazos93@gmail.com>
2026-01-06 09:20:45 -0800

    Upon Play, each obstacle changes to a different size

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    
     void Start()
     {
-        
+        float randomSize = Random.Range(0.5f, 2.0f);
+        transform.localScale = new Vector3(randomSize, randomSize, 1);
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
