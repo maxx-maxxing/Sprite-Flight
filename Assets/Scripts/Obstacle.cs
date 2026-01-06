8634634ce82b09f2bd673d8fe6808eed12559ec2
maxx-maxxing <maxxlazos93@gmail.com>
2026-01-06 10:20:14 -0800

    Adding physics, almost done. Stopping for day

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float minSize = 0.5f;
+    public float maxSize = 2.0f;
+    public float minSpeed = 50f;
+    public float maxSpeed = 150f;
     
     
     void Start()
     {
-        float randomSize = Random.Range(0.5f, 2.0f);
+        float randomSize = Random.Range(minSize, maxSize);
         transform.localScale = new Vector3(randomSize, randomSize, 1);
+        rb =  GetComponent<Rigidbody2D>();
+        float randomSpeed = Random.Range(minSpeed, maxSpeed);
+        rb.AddForce(Vector2.right * randomSpeed); // Could expand to create an array or enum to randomize direction and amount. NO MAGIC NUMBERS
     }
 
     
