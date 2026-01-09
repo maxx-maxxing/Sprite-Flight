3f2659189b4ec8882d2fbfd53afcb1a00c594444
maxx-maxxing <maxxlazos93@gmail.com>
2026-01-09 07:56:56 -0700

    Rebuilt the project up to the current point from memory. Getting cr4cked. Moving on.

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -8,15 +8,19 @@ public class Obstacle : MonoBehaviour
     public float minSpeed = 50f;
     public float maxSpeed = 150f;
     public float maxTorque = 10f;
-    
+
+
+    void Awake()
+    {
+        rb =  GetComponent<Rigidbody2D>(); // ref to Obstacle's physics component
+    }
     
     void Start()
     {
         float randomSize = Random.Range(minSize, maxSize); // variable editable in inspector
         transform.localScale = new Vector3(randomSize, randomSize, 1);
-        rb =  GetComponent<Rigidbody2D>(); // ref to Obstacle's physics component
         float randomSpeed = Random.Range(minSpeed, maxSpeed) / randomSize; // variable editable in inspector
-        Vector2 randomDirection = Random.insideUnitCircle;
+        Vector2 randomDirection = Random.insideUnitCircle.normalized;
         rb.AddForce(randomDirection * randomSpeed);
         rb.AddTorque(Random.Range(-maxTorque, maxTorque));
     }
