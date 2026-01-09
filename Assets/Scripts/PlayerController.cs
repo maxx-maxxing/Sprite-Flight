7d21079e02337a64e9bc2a0cb64bdcb35abf09f6
maxx-maxxing <maxxlazos93@gmail.com>
2026-01-09 08:52:58 -0700

    Added player sprite and started PlayerController sprite

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mouse.current.leftButton.isPressed)
+        {
+            Debug.Log("Left Button pressed");
+        }
+    }
+}
