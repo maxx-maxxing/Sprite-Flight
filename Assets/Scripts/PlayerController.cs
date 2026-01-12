31c467be01a4cfef8c3420a146f0dc55ff3aa445
maxx-maxxing <maxxlazos93@gmail.com>
2026-01-12 10:08:21 -0700

    Moving on to collisions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,25 +3,34 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float thrustForce = 1f;
+    Rigidbody2D rb;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         {
-            if (Mouse.current.leftButton.IsPressed())
+            if(Mouse.current.leftButton.IsPressed())
                 // ^^ When left mouse button pressed, the GameObject needs to rotate to point where you are clicking
             {
                 Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
                 // ^^ Use the Camera to convert mouse/pixel location to world coordinates
-                Vector2 direction = mousePos - transform.position;
-                // ^^ We need direction to be a positive vector, so we MUST subtract the GameObject's world pos. FROM the mouse's world pos.
+                Vector2 direction = (mousePos - transform.position).normalized;
+                /* ^^ We need direction to be a positive vector, so we MUST subtract the
+                 GameObject's world pos. FROM the mouse's world pos. Then we normalize the value
+                 to keep the strength of force independent of mouse-to-object distance
+                 */
                 transform.up = direction;
                 // ^^ The GameObjects Y will always = the positive vector calculated with each LMB press
+                rb.AddForce(direction * thrustForce);
+                // ^^ Push object the direction it's facing by thrustForce
+                
             }    
              
             
